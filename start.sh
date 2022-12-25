9a08e00f337c5ad9599c950cc4fe840fd242aa2a
sachisenapati4 <121351836+sachisenapati4@users.noreply.github.com>
2022-12-26 04:30:06 +0530

    Okkk

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincessV2.git /LazyPrincessV2
+  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincessV2
+  git clone $UPSTREAM_REPO /DQ-The-File-Donor
 fi
-cd /LazyPrincessV2
+cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting DQ-The-File-Donor...."
 python3 bot.py
